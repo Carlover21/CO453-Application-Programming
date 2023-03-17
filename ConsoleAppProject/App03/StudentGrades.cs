7aeffe0bffba213443bc592283deb0ea3bc8e064
Carlover21 <82425897+Carlover21@users.noreply.github.com>
2023-03-17 03:55:37 +0000

    added all the strings needed for the student app and some methods

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using ConsoleAppProject.Helpers;
@@ -9,8 +10,31 @@ namespace ConsoleAppProject.App03
     /// At the moment this class just tests the
     /// Grades enumeration names and descriptions
     /// </summary>
-    public class StudentGrades
+    public class StudentMarks
     {
+        public string Name;
+        public string Surname;
+        public int Marks;
+        public string A, B, C, D, F;
+        public const int Max_Students = 10;
 
+        public void run()
+        {
+            Console.WriteLine("    Welcome to Student mark app by Rayan Hamour    ";
+            ConnvertToMarks();
+            Name();
+            GetStudentMarks();
+        }
+        public void GetStudentMarks()
+        {
+            List<StudentMarks> studentMarksList = new List<StudentMarks>();
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            double mean = 0;
+            for (int i=0; i<Max_Students; i++)
+            {
+                Console.WriteLine(""
+            }
+        
+        }
     }
-}
