8e6d7e1a40102b9534e5043c77bf1a8e9bd2d136
Carlover21 <82425897+Carlover21@users.noreply.github.com>
2023-03-31 12:11:39 +0100

    fixes

--- ConsoleAppProject/App04/NewsFeed.cs
+++ ConsoleAppProject/App04/NewsFeed.cs
@@ -15,7 +15,7 @@ namespace ConsoleAppProject.App04
     /// search or ordering functions.
     ///</summary>
     ///<author>
-    ///  Michael Kölling and David J. Barnes
+    ///  Rayan Hamour
     ///  version 0.1
     ///</author> 
     public class NewsFeed
